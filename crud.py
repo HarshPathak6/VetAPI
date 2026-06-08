1ac347852c6c64463aeaed0dde65245d848d5564
HarshPathak6 <raidenyt6@gmail.com>
2026-06-09 02:32:42 +0530

    Add Pet Delete Operation

--- crud.py
+++ crud.py
@@ -18,3 +18,37 @@ def create_pet(db: Session, pet: PetCreate):
     db.refresh(db_pet)
 
     return db_pet
+
+def get_pet(db: Session, pet_id: int):
+    return db.query(Pet).filter(Pet.id == pet_id).first()
+
+def update_pet(db: Session, pet_id: int, pet_data: PetCreate):
+    pet = db.query(Pet).filter(Pet.id == pet_id).first()
+
+    if pet is None:
+        return None
+
+    pet.name = pet_data.name
+    pet.species = pet_data.species
+    pet.breed = pet_data.breed
+    pet.age = pet_data.age
+    pet.owner_name = pet_data.owner_name
+    pet.owner_phone = pet_data.owner_phone
+
+    db.commit()
+    db.refresh(pet)
+
+    return pet
+
+
+def delete_pet(db: Session, pet_id: int):
+    pet = db.query(Pet).filter(Pet.id == pet_id).first()
+
+    if pet is None:
+        return None
+
+    db.delete(pet)
+    db.commit()
+
+    return pet
+    
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import engine, get_db
 from models import Base
 from schemas import PetCreate, PetResponse
-from crud import create_pet
+from crud import create_pet, get_pet, get_pet, update_pet, delete_pet
 
 app = FastAPI()
 
@@ -16,8 +16,35 @@ def home():
 
 
 @app.post("/pets", response_model=PetResponse)
-def add_pet(
+def add_pet(pet: PetCreate, db: Session = Depends(get_db)):
+    return create_pet(db, pet)
+
+
+@app.get("/pets/{pet_id}", response_model=PetResponse)
+def read_pet(pet_id: int, db: Session = Depends(get_db)):
+    pet = get_pet(db, pet_id)
+
+    if pet is None:
+        raise HTTPException(status_code=404, detail="Pet not found")
+
+    return pet
+
+@app.put("/pets/{pet_id}", response_model=PetResponse)
+def edit_pet(
+    pet_id: int,
     pet: PetCreate,
     db: Session = Depends(get_db)
 ):
-    return create_pet(db, pet)
+    updated_pet = update_pet(
+        db,
+        pet_id,
+        pet
+    )
+
+    if updated_pet is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Pet not found"
+        )
+
+    return updated_pet
