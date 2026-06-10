f97e47c1a641694b87e1ffbbb3c4b275e5c9ef08
HarshPathak6 <raidenyt6@gmail.com>
2026-06-10 19:57:29 +0530

    Updated config file to fix inconsistency

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 # Loads values from .env file
-from pydantic_settings import BaseSettings
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
 
-    class Config:
-        env_file = ".env"
-
+    model_config = SettingsConfigDict(
+        env_file=".env"
+    )
 
 settings = Settings()
