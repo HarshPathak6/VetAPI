c46c5f43c2d41e252072f6ab49f47456c106a93f
HarshPathak6 <raidenyt6@gmail.com>
2026-07-29 01:01:51 +0530

    changed allow origin

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origin_regex=r"https://.*\.lovable\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
